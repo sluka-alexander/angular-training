f7a4f0fa79e86a7b365f46f0ef37746cf1fdad03
unknown <slukasane@gmail.com>
2020-06-23 20:20:00 +0300

    work with components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Training angular';
+  posts: Post[] = [
+    { title: 'Хочу выучить Angular',
+    text: 'Я всё ещё учу компоненты и вообще всё на свете',
+    id: 1,
+    },
+    { title: 'Следующий блок',
+    text: 'Следующий блок будет про директивы и что-то ещё',
+    id: 2,
+    }
+  ];
+  removePost(id: number) {
+    this.posts = this.posts.filter(p=> {
+      return p.id !== id;
+    })
+  }
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+  }
 }
