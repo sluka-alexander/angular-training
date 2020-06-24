839bb910515af423ea163ac0ceaee776f87fbf8a
unknown <slukasane@gmail.com>
2020-06-24 19:29:43 +0300

    pipe filter

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
-export interface Post {
+export  interface Post {
   title: string
   text: string
-  id?: number
 }
 
 @Component({
@@ -13,21 +12,9 @@ export interface Post {
 })
 export class AppComponent {
   posts: Post[] = [
-    { title: 'Хочу выучить Angular',
-    text: 'Я всё ещё учу компоненты и вообще всё на свете',
-    id: 1,
-    },
-    { title: 'Следующий блок',
-    text: 'Следующий блок будет про директивы и что-то ещё',
-    id: 2,
-    }
+    {title: 'Asus', text: 'Лучший ноутбук'},
+    {title: 'JS', text: 'Лучший язык программирования'},
+    {title: 'MongoDB', text: 'Лучшая База Данных'},
   ];
-  removePost(id: number) {
-    this.posts = this.posts.filter(p=> {
-      return p.id !== id;
-    })
-  }
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-  }
+  search: string = '';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
+import { FilterPipe } from './pape/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent,
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
